d4b1bb1c9e18312b72daece79e0d90d39ccad125
Daniel Sposito <daniel.g.sposito@gmail.com>
2016-03-11 04:09:57 -0800

    Updates the base controller to always inject the request object.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
+/**
+ * Serves as the base API controller.
+ */
 class Controller extends BaseController
 {
-    //
+    /**
+     * The dependency-injected request object.
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * Initializes the class.
+     *
+     * @param Request $request The dependency-injected request object.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
 }
